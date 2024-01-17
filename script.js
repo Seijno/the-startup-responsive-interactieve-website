21b9bd0a527681bb2d95557b351bc72848bf7bf8
Seijno <seijnovannieuwkoop@gmail.com>
2024-01-17 11:00:47 +0100

    functie uitbreiding en media query

--- script.js
+++ script.js
@@ -13,10 +13,13 @@ jsButton.addEventListener('click', changejs );
 
 function changehtml(){
   htmlBar.classList.toggle('htmlToggle');
+  htmlButton.classList.toggle('htmlButton')
 }
 function changecss(){
     cssBar.classList.toggle('cssToggle');
+    cssButton.classList.toggle('cssButton')
   }
   function changejs(){
     jsBar.classList.toggle('jsToggle');
+    jsButton.classList.toggle('jsButton')
   }
