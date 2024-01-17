9e1eb93492c245622af79b06bd0d1bc908390ed2
Seijno <seijnovannieuwkoop@gmail.com>
2024-01-17 10:41:39 +0100

    functie afgemaakt

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let htmlButton = document.getElementById('htmlBtn');
-let htmlBar = document.getElementById('html');
+var htmlButton = document.getElementById('htmlBtn');
+var htmlBar = document.querySelector('.htmlbar');
+
+var cssButton = document.getElementById('cssBtn');
+var cssBar = document.querySelector('.cssbar');
+
+var jsButton = document.getElementById('jsBtn');
+var jsBar = document.querySelector('.jsbar');
 
 htmlButton.addEventListener('click', changehtml );
+cssButton.addEventListener('click', changecss );
+jsButton.addEventListener('click', changejs );
 
 function changehtml(){
   htmlBar.classList.toggle('htmlToggle');
 }
+function changecss(){
+    cssBar.classList.toggle('cssToggle');
+  }
+  function changejs(){
+    jsBar.classList.toggle('jsToggle');
+  }
