1a6b601b050dad470d1c81e48eda1ee89b096912
Seijno <seijnovannieuwkoop@gmail.com>
2024-01-23 22:43:23 +0100

    commentaar en hervorming

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// alle variabels
 const htmlButton = document.getElementById('htmlBtn');
 const htmlBar = document.querySelector('.htmlbar');
 
@@ -7,10 +8,12 @@ const cssBar = document.querySelector('.cssbar');
 const jsButton = document.getElementById('jsBtn');
 const jsBar = document.querySelector('.jsbar');
 
+// vertel dat er moet worden gereageerd op de knoppen 
 htmlButton.addEventListener('click', changehtml );
 cssButton.addEventListener('click', changecss );
 jsButton.addEventListener('click', changejs );
 
+// zorgt voor de class shift in de styling en displays
 function changehtml(){
   htmlBar.classList.toggle('htmlToggle');
   htmlButton.classList.toggle('htmlButton')
